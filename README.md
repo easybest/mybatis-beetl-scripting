a94919f5d90b8e7a2d72902af6cec9a5f3dbd2f4
JARVIS SONG <iamjarvissong@gmail.com>
2016-12-19 22:33:54 +0800

    basic fun

---
+++ README.md
@@ -0,0 +1,74 @@
+# MyBatis Beetl Support  [![Build Status](https://travis-ci.org/hatunet/mybatis-beetl-scripting.svg?branch=master)](https://travis-ci.org/hatunet/mybatis-beetl-scripting) 
+
+
+## Introduction 
+
+mybatis-beetl is an extension that allows you to use the Beetl scripting language to generate your dynamic SQL queries on the fly.
+
+If you are not familiar with beetl, you can learn it from its documentation site:
+
+[Beetl Reference](http://ibeetl.com/guide/#beetl)
+
+## Install
+
+1. Put the mybatis-beetl-scripting-<version>.jar in your classpath.
+
+    If you are using maven, just add a dependency to:
+```
+GroupId: com.ifrabbit
+ArtifactId: mybatis-beetl-scripting
+```
+
+2. Register the language driver alias in your mybatis configuration file:
+```xml
+<configuration>
+  ...
+  <typeAliases>
+    <typeAlias alias="beetl" type="org.mybatis.scripting.beetl.Driver"/>
+  </typeAliases>
+  ...
+</configuration>
+
+```
+
+3. Optional: Set the beetl as your default scripting language:
+```xml
+<configuration>
+  ...
+  <settings>
+    <setting name="defaultScriptingLanguage" value="beetl"/>
+  </settings>
+  ...
+</configuration>
+
+```
+
+## Usage
+
+Just write your dynamic queries and use beetl:
+
+Example:
+
+
+```xml
+<select id="findPerson" lang="beetl">
+  SELECT *
+  FROM person
+  WHERE name LIKE @{pattern, jdbcType=VARCHAR}
+</select>
+```
+
+
+
+
+
+
+## Contributing to Spring Data MyBatis
+
+Here are some ways for you to get involved in the community:
+
+* Github is for social coding: if you want to write code, we encourage contributions through pull requests from [forks of this repository](http://help.github.com/forking/). 
+
+## Help me better - Donation
+[![paypal](https://www.paypal.com/en_US/i/btn/x-click-butcc-donate.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=W7PLNCBK5K8JS)
+
---
+++ src/main/java/org/mybatis/scripting/beetl/JoinFunction.java
@@ -0,0 +1,79 @@
+/*
+ *
+ *   Copyright 2016 the original author or authors.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
+package org.mybatis.scripting.beetl;
+
+import org.apache.ibatis.mapping.ParameterMapping;
+import org.apache.ibatis.session.Configuration;
+import org.beetl.core.Context;
+import org.beetl.core.Function;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * JoinFunction.
+ */
+public class JoinFunction implements Function {
+    @Override
+    public Object call(Object[] paras, Context ctx) {
+        Object[] objs;
+        Object temp = paras[0];
+        if (temp instanceof Object[]) {
+            objs = (Object[]) temp;
+        } else if (temp.getClass().isArray()) {
+            int len = Array.getLength(temp);
+            objs = new Object[len];
+            for (int i = 0; i < len; i++) {
+                objs[i] = Array.get(temp, i);
+            }
+        } else if (temp instanceof Collection) {
+            objs = ((Collection) temp).toArray();
+        } else {
+            throw new RuntimeException("join.paras[0] must be a collection or an array!");
+        }
+
+        Configuration configuration = (Configuration) ctx.getGlobal("_configuration");
+        List<ParameterMapping> parameterMappings = (List<ParameterMapping>) ctx.getGlobal("_parameterMappings");
+        Map<String, Object> params = (Map<String, Object>) ctx.getGlobal("_params");
+
+        int c = 0;
+        StringBuilder builder = new StringBuilder();
+        for (Object obj : objs) {
+            builder.append("?,");
+            params.put("_JOIN_C_" + c, obj);
+
+            ParameterMapping parameterMapping = new ParameterMapping.Builder(configuration, "_JOIN_C_" + c, null == obj ? Object.class : obj.getClass()).build();
+            parameterMappings.add(parameterMapping);
+            c++;
+        }
+        if (builder.length() > 0) {
+            builder.deleteCharAt(builder.length() - 1);
+        }
+        try {
+            ctx.byteWriter.writeString(builder.toString());
+        } catch (IOException e) {
+        }
+
+        return null;
+    }
+
+}
