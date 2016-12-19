de679bc95a0f785fb2647ef429547a07707cb777
Jarvis <hapcars@gmail.com>
2016-12-19 22:33:54 +0800

    basic fun

---
+++ src/main/java/org/mybatis/scripting/beetl/SQLVarRef.java
@@ -0,0 +1,29 @@
+package org.mybatis.scripting.beetl;
+
+import org.beetl.core.Context;
+import org.beetl.core.statement.VarRef;
+
+/**
+ * @author jarvis@ifrabbit.com
+ * @date 16/3/16
+ */
+public class SQLVarRef extends VarRef {
+
+    public SQLVarRef(VarRef ref) {
+        super(ref.attributes, ref.hasSafe, ref.safe, ref.token, ref.token);
+        this.varIndex = ref.varIndex;
+    }
+
+    @Override
+    public Object evaluate(Context ctx) {
+
+
+        Object value = ctx.vars[varIndex];
+        if (value == Context.NOT_EXIST_OBJECT) {
+            ctx.getGlobal("_root");
+
+        }
+        return super.evaluate(ctx);
+    }
+
+}
